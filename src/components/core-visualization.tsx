36dbd2e45c24940f748e3e3743cd06614f09b5d6
조난영 <chonanyoung25@gmail.com>
2026-01-20 04:43:02 +0000

    가운데 돌아가는 파란색 원을 입체적으로 돌아가도록 할 수 없어?? 지금 너무 평면적으로 돌아가는데?

--- src/components/core-visualization.tsx
+++ src/components/core-visualization.tsx
@@ -4,7 +4,12 @@ const CoreVisualization = () => {
   return (
     <div className="visualization-container">
       <div className="core-sphere">
-         {/* Particles for sparkle effect */}
+        {/* Inner rings for 3D effect */}
+        <div className="core-ring" style={{ transform: 'rotateY(20deg) rotateX(70deg)' }}></div>
+        <div className="core-ring" style={{ transform: 'rotateY(50deg) rotateX(40deg)' }}></div>
+        <div className="core-ring" style={{ transform: 'rotateY(80deg) rotateX(10deg)' }}></div>
+
+        {/* Particles for sparkle effect */}
         <div className="particle" style={{ top: '25%', left: '40%', animationDelay: '0s' }} />
         <div className="particle" style={{ top: '50%', left: '75%', animationDelay: '-1s' }} />
         <div className="particle" style={{ top: '75%', left: '30%', animationDelay: '-2s' }} />
