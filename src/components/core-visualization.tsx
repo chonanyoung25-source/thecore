56a53fe4043f74270c226ca75084b79ac3ef0fdf
조난영 <chonanyoung25@gmail.com>
2026-01-20 04:38:55 +0000

    보내준 이미지를 보고 다시 구현해봐....너무 평면적이고 밋밋해.

--- src/components/core-visualization.tsx
+++ src/components/core-visualization.tsx
@@ -3,14 +3,38 @@
 const CoreVisualization = () => {
   return (
     <div className="visualization-container">
-      <div className="core-sphere" />
-      <div className="orbit orbit-1">
+      <div className="core-sphere">
+         {/* Particles for sparkle effect */}
+        <div className="particle" style={{ top: '25%', left: '40%', animationDelay: '0s' }} />
+        <div className="particle" style={{ top: '50%', left: '75%', animationDelay: '-1s' }} />
+        <div className="particle" style={{ top: '75%', left: '30%', animationDelay: '-2s' }} />
+      </div>
+
+      {/* Geodesic Wireframe */}
+      <div className="geo-sphere">
+        <div className="geo-ring" style={{ transform: 'rotateY(0deg) rotateX(15deg)' }}></div>
+        <div className="geo-ring" style={{ transform: 'rotateY(30deg) rotateX(15deg)' }}></div>
+        <div className="geo-ring" style={{ transform: 'rotateY(60deg) rotateX(15deg)' }}></div>
+        <div className="geo-ring" style={{ transform: 'rotateY(90deg) rotateX(15deg)' }}></div>
+        <div className="geo-ring" style={{ transform: 'rotateY(120deg) rotateX(15deg)' }}></div>
+        <div className="geo-ring" style={{ transform: 'rotateY(150deg) rotateX(15deg)' }}></div>
+        
+        <div className="geo-ring" style={{ transform: 'rotateX(90deg) rotateY(0deg) rotateZ(15deg)' }}></div>
+        <div className="geo-ring" style={{ transform: 'rotateX(90deg) rotateY(30deg) rotateZ(15deg)' }}></div>
+        <div className="geo-ring" style={{ transform: 'rotateX(90deg) rotateY(60deg) rotateZ(15deg)' }}></div>
+        <div className="geo-ring" style={{ transform: 'rotateX(90deg) rotateY(90deg) rotateZ(15deg)' }}></div>
+        <div className="geo-ring" style={{ transform: 'rotateX(90deg) rotateY(120deg) rotateZ(15deg)' }}></div>
+        <div className="geo-ring" style={{ transform: 'rotateX(90deg) rotateY(150deg) rotateZ(15deg)' }}></div>
+      </div>
+
+      {/* Orbits and Satellites */}
+      <div className="orbit-path orbit-1">
         <div className="satellite satellite-1" />
       </div>
-      <div className="orbit orbit-2">
+      <div className="orbit-path orbit-2">
         <div className="satellite satellite-2" />
       </div>
-      <div className="orbit orbit-3">
+      <div className="orbit-path orbit-3">
         <div className="satellite satellite-3" />
       </div>
     </div>
